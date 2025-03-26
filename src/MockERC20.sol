ced2e9784729386546376b026e59963f88ee2092
Mark Muskardin <markmuskardin@gmail.com>
2025-03-26 12:44:17 -0700

    forge install: openzeppelin-contracts
    v5.2.0

--- src/MockERC20.sol
+++ src/MockERC20.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.26;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockERC20 is ERC20, ERC20Pausable, ERC20Burnable, Ownable {
